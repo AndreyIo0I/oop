adc2763b92c32d2af35bfc68768281f8d746f6ee
AndreyArsbkv <andrbeckov@gmail.com>
2021-02-10 22:26:41 +0300

    commit1 попробуем собрать в vs

---
+++ 1/1.cpp
@@ -0,0 +1,12 @@
+/*
+	Программа, выводящая строку "Hello, world!" в стандартный поток вывода
+*/
+
+#include <iostream>
+
+int main()
+{
+    // Вывод в стандартный поток вывода
+    std::cout << "Wooooppapa" << std::endl;
+    return 0;
+}
