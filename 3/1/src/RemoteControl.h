5cc1397dfb891d1e494334b955034505a34edb32
andrb <andrbeckov@gmail.com>
2021-06-21 10:54:22 +0300

    3-1

---
+++ 3/1/src/RemoteControl.h
@@ -0,0 +1,30 @@
+﻿#include <map>
+#include <functional>
+#include <iosfwd>
+#include <string>
+
+class CTVSet;
+
+class CRemoteControl
+{
+public:
+	CRemoteControl(CTVSet & tv, std::istream & input, std::ostream & output);
+	bool HandleCommand();
+
+private:
+	bool TurnOn(std::istream & args);
+	bool TurnOff(std::istream & args);
+	bool Info(std::istream & args);
+	bool SelectChannel(std::istream & args);
+private:
+	using Handler = std::function<bool(std::istream & args)>;
+
+	using ActionMap = std::map<std::string, Handler>;
+
+	CTVSet & m_tv;
+	std::istream & m_input;
+	std::ostream & m_output;
+
+	const ActionMap m_actionMap;
+};
+
