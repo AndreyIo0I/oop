c337dbd6e02aa334b0ed84c88d01378f0671be59
Andrey Arsbkv <andrbeckov@gmail.com>
2021-10-11 10:11:51 +0300

    2-3

--- 2/3/src/Dictionary.h
+++ 2/3/src/Dictionary.h
@@ -7,6 +7,7 @@ class Dictionary
 public:
 	Dictionary(const std::string& filename);
 	void Run(std::istream&, std::ostream&);
+private:
 	std::map<std::string, std::string> m_translations;
 	std::string m_filename;
 };
