2c39cf4eee9df20b7ac0ff0da37377df9302d2bb
Andrey Arsbkv <andrbeckov@gmail.com>
2021-10-11 10:09:32 +0300

    2-3

--- 2/3/main.cpp
+++ 2/3/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
 {
 	if (argc != 2)
 	{
-		cout << "Передайте только название файла\n";
+		cout << "specify filename\n";
 		return 0;
 	}
 	string filename = argv[1];
