14f3eb961bf1010cdb53652b18bdb4d0962a7adf
Andrey Arsbkv <andrbeckov@gmail.com>
2021-10-24 16:25:31 +0300

    2-4 подправил тесты

--- 2/4/tests/test.cpp
+++ 2/4/tests/test.cpp
@@ -4,9 +4,20 @@
 
 TEST_CASE("getPrimeNumbers")
 {
-	std::set<int> set1 = {2};
-	CHECK(getPrimeNumbers(2) == set1);
-	std::set<int> set2 = {2, 3, 5, 7};
-	CHECK(getPrimeNumbers(9) == set2);
-	CHECK(getPrimeNumbers(100000000).size() == 5761455);
+	SECTION("get one prime number")
+	{
+		std::set<int> set1 = {2};
+		CHECK(getPrimeNumbers(2) == set1);
+	}
+
+	SECTION("get some prime numbers")
+	{
+		std::set<int> set2 = {2, 3, 5, 7};
+		CHECK(getPrimeNumbers(9) == set2);
+	}
+
+	SECTION("get a lot of prime numbers")
+	{
+		CHECK(getPrimeNumbers(100000000).size() == 5761455);
+	}
 }
